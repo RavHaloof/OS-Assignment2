33094c94d6f25113bf84c189c61c17f8a29d49a9
Avi Ben David <avi@brandale.co.il>
2025-05-10 09:18:10 +0300

    added read function, and the basic read through requests file loop

--- Exercise_2/file_processor.c
+++ Exercise_2/file_processor.c
@@ -7,9 +7,39 @@
 #include <fcntl.h>
 #include <string.h>
 
-// Function which will read the file
-void readFile() {
+#define BUFFER 1000
 
+// Checks the length of an input in a file
+int checkFileLength(FILE** data) {
+    // Resets the current pointer to the file back to the start
+    fseek(*data, 0, SEEK_SET);
+    char input[BUFFER];
+    fscanf(*data, "%s", input);
+    // Resets the current pointer to the file back to the start after we checked its length
+    fseek(*data, 0, SEEK_SET);
+    return strlen(input);
+}
+
+// Function which will read the file
+void readFile(FILE **srcFile, FILE **destFile, int startOffset, int endOffset) {
+    //Checking edge cases:
+    if (startOffset > endOffset) {
+        printf("The start offset is greater than the end offset. why u bully me? :(\n");
+        return;
+    }
+    if (startOffset < 0) {
+        printf("The start offset is lesser than 0. why u bully me? :(\n");
+        return;
+    }
+    if (endOffset > checkFileLength(srcFile)) {
+        printf("%d\n", checkFileLength(srcFile));
+        printf("%d\n", endOffset);
+        printf("The end offset is greater than the length of the data file. why u bully me? :(\n");
+        return;
+    }
+    char read[BUFFER];
+    fscanf(*srcFile, "%s", read);
+    printf("%s\n", read);
 }
 
 // Function which will write into the file
@@ -42,7 +72,13 @@ int main(int argc, char *argv[]) {
     if (requests == NULL) {
         perror("requests.txt\n");
         exit(1);
-    };
+    }
+
+    // Main reading through requests file loop
+    char input[BUFFER];
+    while (fscanf(requests, "%s ", input) != EOF) {
+        printf("%s\n", input);
+    }
 
     printf("hi\n");
 }
