5bcc163e2f16165870b7a47a713a1cd1b0c0f41e
Avi Ben David <avi@brandale.co.il>
2025-05-11 11:05:47 +0300

    Tests completed, and added comments, part 3 done!

--- Exercise_2/backup.c
+++ Exercise_2/backup.c
@@ -10,14 +10,16 @@
 
 #define BUFFER 1024
 
+// Function which creates a hard link between two files
 void createHardlink(const char *src, const char *dst) {
     if (link(src, dst) == -1) {
         perror("hardlink");
     }
 }
+// Function which creates a symlink between two files
 void copySymlink(const char *src, const char *dst) {
     char target[BUFFER];
-    ssize_t len = readlink(src, target, sizeof(target) - 1);
+    int len = readlink(src, target, sizeof(target) - 1);
     if (len == -1) {
         perror("readlink");
         return;
@@ -27,6 +29,7 @@ void copySymlink(const char *src, const char *dst) {
         perror("symlink");
     }
 }
+// Function which creates a directory, when given a path
 void copyDirectory(const char *dst) {
     mkdir(dst, 0777);
 }
@@ -48,27 +51,34 @@ void scanDirectory(const char *srcPath, const char *dstPath) {
         if (strcmp(rootDir->d_name, ".") == 0 || strcmp(rootDir->d_name, "..") == 0) {
             continue;
         }
+        // Creating a string which will have the full path of our current source directory and dst directory
         char srcFull[BUFFER], dstFull[BUFFER];
-
         snprintf(srcFull, BUFFER, "%s/%s", srcPath, rootDir->d_name);
         snprintf(dstFull, BUFFER, "%s/%s", dstPath, rootDir->d_name);
 
-
+        // Checking whether stat worked
         if (lstat(srcFull, &sb) == -1) {
             perror("stat failed");
             continue;
         }
-
+        // Now checking what type of file did we encounter
+        // In case of a directory, copy it to the destination, and run a recursion from that directory
         if (S_ISDIR(sb.st_mode)) {
             copyDirectory(dstFull);
             printf("Directory: %s\n", srcFull);
             scanDirectory(srcFull, dstFull);
-        } else if (S_ISLNK(sb.st_mode)) {
+        }
+        // In case of a symlink, creates a symlink
+        else if (S_ISLNK(sb.st_mode)) {
             copySymlink(srcFull, dstFull);
-        } else if (S_ISREG(sb.st_mode)) {
+        }
+        // In case of a regular file (hard link) creates a hard link
+        else if (S_ISREG(sb.st_mode)) {
             createHardlink(srcFull, dstFull);
-        } else {
-            printf("Skipping: %s\n", srcFull);
+        }
+        // If it's not any of the above, idk what to do
+        else {
+            continue;
         }
     }
     closedir(srcDir);
@@ -93,7 +103,7 @@ int main(int argc, char *argv[]) {
         perror("backup dir\n");
         exit(1);
     }
-    // Opening the backup directory we created
+    // Running the main recourse function
     scanDirectory(argv[1], argv[2]);
     return 0;
 }
